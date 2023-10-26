c04feaf73aed6d36eeec2f5a84a3cc84c2e1afc2
donaghru <146995519+donagheyruairi@users.noreply.github.com>
2023-10-26 16:04:27 +0100

    Full app created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import { useState } from "react";
 
+function WeatherDay(data) {
+  return (
+    data.rain === true ?(
+    <div className='weatherModule'>
+      <h4>Day {data.day}</h4>
+      <h4>Forecasted: Rain</h4>
+      <h4>Average: temp of { data.temp }° wind speed of { data.wind }m/s</h4>
+    </div>
+  ):(
+    <div className='weatherModuleDry'>
+      <h4>Day {data.day}</h4>
+      <h4>Forecasted: Dry</h4>
+      <h4>Average: temp of { data.temp }° wind speed of { data.wind }m/s</h4>
+    </div>
+  ));
+}
+
 function App() {
   const [country, setCountry] = useState("");
   const [data, setData] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   const changeHandler = (event) => {
     setCountry(event.target.value);
@@ -12,9 +30,10 @@ function App() {
   const keyDownHandler = async (event) => {
     if(event.key === "Enter") {
       try{
-        const response = await fetch("/api/"+country); 
+        const response = await fetch("/api/"+country);
         var fetchedData = await response.json();
-        setData(fetchedData);
+        setData(fetchedData.countryData);
+        setWeather(fetchedData.weatherData);
       } catch (error) {
         console.error(error);
       }
@@ -26,8 +45,64 @@ function App() {
       <div className="central">
         <h1>Welcome to the Internship Application Country App</h1>
         <input type="text" placeholder="Enter country here" value={country} onChange={changeHandler} onKeyDown={keyDownHandler}></input>
+        { data == null &&
+          <h3 id='blankDisplay'>This area seems pretty empty. Search for a country so we can show some data!</h3>
+        }
         { data != null &&
-          <p>{data.name.common}</p>
+          <div className="dataSection">
+
+            <div className='textSection' >
+              <div className="countryDetails">
+                <div className="flagContainer">
+                  <img className="flagImage" src={data.flags.png} alt={data.flags.alt}></img>
+                </div>
+                <div className="placeNames">  
+                    <h3>{data.name.common}</h3>
+                    <h3><i>{data.name.official}</i></h3>
+                </div>
+              </div>
+
+              <div className='factSection'>
+                { data.capital==null &&
+                  <h4> Did you know that {data.name.common} does not have a capital?</h4>
+                }
+                { data.capital!=null && data.capital.length === 2 &&
+                  <h4> Did you know that {data.name.common} has 2 capital cities?
+                  These are: {data.capital[0]} and {data.capital[1]}!</h4>
+                }
+                { data.capital!=null && data.capital.length === 3 &&
+                  <h4> Did you know that {data.name.common} has 3 capital cities?
+                  These are: {data.capital[0]}, {data.capital[1]} and {data.capital[2]}!</h4>
+                }
+                <h4>{data.name.common} is {data.independent ? "an independent " : "a non-independent "} 
+                    nation with a population of {data.population}.</h4>
+                <h4>{data.name.common} is in the {data.subregion} subregion of {data.region}.</h4>
+                <h4>It has a total area of {data.area}km²,
+                  and is{data.unMember ? "" : " not"} a current member of the UN.</h4>
+                <h3>Planning to go to {data.name.common}?</h3>
+                <h4>Remember that {data.demonyms.eng.m} people drive on the {data.car.side}.</h4>
+                <h4>They use {data.currencies[Object.keys(data.currencies)[0]].name}  here, so 
+                  make sure to exchange some for your trip.</h4>
+              </div>
+            </div>
+              
+            <div className='weatherSection'>
+              { data.capital!=null &&
+                  <h4> Here's the weather for the next 5 days in the capital city: {data.capital[0]}!</h4>
+              }
+              { data.capital==null &&
+                  <h4> Here's the weather for the next 5 days in {data.name.common}!</h4>
+              }
+              <div className='weatherModules'>
+                <WeatherDay day="1" rain={weather.days[0].rain} temp={weather.days[0].temp} wind={weather.days[0].wind} />
+                <WeatherDay day="2" rain={weather.days[1].rain} temp={weather.days[1].temp} wind={weather.days[1].wind} />
+                <WeatherDay day="3" rain={weather.days[2].rain} temp={weather.days[2].temp} wind={weather.days[2].wind} />
+                <WeatherDay day="4" rain={weather.days[3].rain} temp={weather.days[3].temp} wind={weather.days[3].wind} />
+                <WeatherDay day="5" rain={weather.days[4].rain} temp={weather.days[4].temp} wind={weather.days[4].wind} />
+              </div>
+            </div>
+
+          </div>
         }
       </div>
     </div>
--- server/server.js
+++ server/server.js
@@ -8,15 +8,54 @@ app.use(express.urlencoded({ extended:true }))
 app.use(express.static(path.join(__dirname, '../client/build')))
 const port = process.env.PORT || 8082;
   
+function weatherSimplify(timeStamps){
+  var rain = false;
+  var tempSum = 0;
+  var windSum = 0;
+  for(let i=0; i<8; i++){
+    if(timeStamps[i].weather[0].main=="Rain") rain=true;
+    tempSum+=timeStamps[i].main.temp;
+    windSum+=timeStamps[i].wind.speed;
+  }
+  return {
+    rain: rain,
+    temp: (tempSum/8).toFixed(2),
+    wind: (windSum/8).toFixed(2)
+  };
+}
+
 app.get("/api/:country?",async function(req,res){
     console.log(req.params.country)
     try {
         var country = await axios.get("https://restcountries.com/v3.1/name/"+req.params.country);
-        res.send(country.data[0]);
+        var countryData = country.data[0];
+        var lat;
+        var lon;
+        if(countryData.capital!=null){
+          lat = countryData.capitalInfo.latlng[0];
+          lon = countryData.capitalInfo.latlng[1];
+        }
+        else{
+          lat = countryData.latlng[0];
+          lon = countryData.latlng[1];
+        }
+        var weather = await axios.get("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=96132c218ae4f3cda2e2ce51b266ac52&units=metric");
+        const weatherData = {
+          days:[
+            weatherSimplify(weather.data.list.slice(0, 8)),
+            weatherSimplify(weather.data.list.slice(8, 16)),
+            weatherSimplify(weather.data.list.slice(16, 24)),
+            weatherSimplify(weather.data.list.slice(24, 32)),
+            weatherSimplify(weather.data.list.slice(32, 40))
+          ]
+        };
+        res.send({
+          countryData: countryData,
+          weatherData: weatherData
+        });
       } catch (error) {
         console.error(error);
-        console.log("Error check")
-        //res.status(500).send('Error: Could not retrieve data');
+        res.status(500).send('Error: Could not retrieve data');
       }
 })
 
